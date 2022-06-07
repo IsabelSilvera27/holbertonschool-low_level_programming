03b862c2fe28b0728aa80d474ba12698e264d71a
IsabelSilvera <isa_18-24@hotmail.com>
2022-06-07 16:26:03 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
