166c2e0a933879c5d8cd614dbe3d2dad4a62be79
IsabelSilvera <isa_18-24@hotmail.com>
2022-06-14 09:50:34 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97 ; ch <= 122 ; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar(10);
+	return (0);
+}
