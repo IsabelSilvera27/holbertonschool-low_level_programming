afde8a9dba753127541bae2b3bcf6923fecdfab3
IsabelSilvera <isa_18-24@hotmail.com>
2022-06-14 09:54:45 -0700

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void);
 {
 	int ch;
 
